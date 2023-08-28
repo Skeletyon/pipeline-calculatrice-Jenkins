315782bd726789b1729ab3671e088eefd5b5478f
Ubuntu <ubuntu@ip-172-31-23-29.eu-west-1.compute.internal>
2023-08-28 16:20:14 +0000

    reformat file

--- sources/calc.py
+++ sources/calc.py
@@ -2,41 +2,45 @@
 La librairie calc permet de faire les opérations basiques de calcul entre deux entiers.
 """
 
-def add(arg1,arg2):
+
+def add(arg1, arg2):
     try:
         return int(arg1)+int(arg2)
-    except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+    except ValueError:
+        print("Un des arguments n'est pas un entier.")
 
-def sous(arg1,arg2):
+
+def sous(arg1, arg2):
     try:
         return int(arg1)-int(arg2)
-    except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+    except ValueError:
+        print("Un des arguments n'est pas un entier.")
 
 
-def mult(arg1,arg2):
+def mult(arg1, arg2):
     try:
         return int(arg1)*int(arg2)
-    except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+    except ValueError:
+        print("Un des arguments n'est pas un entier.")
 
-def div(arg1,arg2):
+
+def div(arg1, arg2):
     try:
         return int(arg1)/int(arg2)
-    except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+    except ValueError:
+        print("Un des arguments n'est pas un entier.")
     except ZeroDivisionError:
         print("Vous divisez par 0.")
 
-def ope(operateur,arg1,arg2):   
-    if operateur=='+':
-        return add(arg1,arg2)        
-    elif operateur=="-":
-        return sous(arg1,arg2)
-    elif operateur=="x":
-        return mult(arg1,arg2)
-    elif operateur=="/":
-        return div(arg1,arg2)
+
+def ope(operateur, arg1, arg2):
+    if operateur == '+':
+        return add(arg1, arg2)
+    elif operateur == "-":
+        return sous(arg1, arg2)
+    elif operateur == "x":
+        return mult(arg1, arg2)
+    elif operateur == "/":
+        return div(arg1, arg2)
     else:
         print("L'opérateur {} n'est pas reconnu.".format(operateur))
